8299f1411ec59cde12eead660d4ce16ff85b26d1
sjun8932 <sjun8932@gmail.com>
2021-06-15 14:12:54 +0900

    모듈 시스템과 웹팩 설정

---
+++ wordplay/WordRelay.jsx
@@ -0,0 +1,15 @@
+const React = require('react');
+const { Component } = React;
+
+class WordRelay extends Component {
+
+    state = {
+
+    };
+
+    render() {
+
+    }
+}
+
+Module.exports = WordRelay;
--- wordplay/webpack.config.js
+++ wordplay/webpack.config.js
@@ -1,3 +1,15 @@
-module.exports = {
+const path = require('path');
 
+module.exports = {
+    name: 'wordrelay-setting',
+    mode: 'development',
+    devtool: 'eval',
+    
+    entry: {
+        app:['.client.jsx'], // client.jsx 안에서 WordRelay를 불러들이고 있으니 배열안에 client.jsx만 넣으면 된다.
+    }, // 입력
+    output: {
+       path: path.join(__dirname, 'dist'), // C:\Users\SANGJUN\Desktop\SANGJUN\Programming\Workspace\vscode_workspace\ReactWebGame\wordplay\dist 이렇게 긴 파일 주소를 짧게 처리
+       filename: 'app.js'
+    } // 출력
 };
