4703797a0f8359dde2d37479a2d6996d419c4a90
sjun8932 <sjun8932@gmail.com>
2021-06-15 15:00:42 +0900

    웹팩으로 빌드하기

--- wordplay/WordRelay.jsx
+++ wordplay/WordRelay.jsx
@@ -4,12 +4,12 @@ const { Component } = React;
 class WordRelay extends Component {
 
     state = {
-
+        text: "웹팩 설정을 한번 해보았다."
     };
 
     render() {
-
+        return <h1>{this.state.text}</h1>
     }
 }
 
-Module.exports = WordRelay;
+module.exports = WordRelay;
--- wordplay/webpack.config.js
+++ wordplay/webpack.config.js
@@ -4,10 +4,25 @@ module.exports = {
     name: 'wordrelay-setting',
     mode: 'development',
     devtool: 'eval',
-    
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+
     entry: {
-        app:['.client.jsx'], // client.jsx 안에서 WordRelay를 불러들이고 있으니 배열안에 client.jsx만 넣으면 된다.
+        app:['./client'], // client.jsx 안에서 WordRelay를 불러들이고 있으니 배열안에 client.jsx만 넣으면 된다.
     }, // 입력
+
+    module: {
+        rules: [{
+            test: /\.jsx?/,
+            loader: 'babel-loader',
+            options: {
+               presets: ['@babel/preset-env', '@babel/preset-react'],
+               plugins: ['@babel/plugin-proposal-class-properties'], 
+            },
+        }],
+    },
+
     output: {
        path: path.join(__dirname, 'dist'), // C:\Users\SANGJUN\Desktop\SANGJUN\Programming\Workspace\vscode_workspace\ReactWebGame\wordplay\dist 이렇게 긴 파일 주소를 짧게 처리
        filename: 'app.js'
