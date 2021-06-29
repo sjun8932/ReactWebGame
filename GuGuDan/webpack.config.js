e2192ee9822355439b0de622f126eac4bf5e11ae
unknown <sjun8932@gmail.com>
2021-06-29 10:34:05 +0900

    웹팩설정

--- GuGuDan/webpack.config.js
+++ GuGuDan/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
 
@@ -18,12 +19,14 @@ module.exports = {
             test: /\.jsx?/,
             loader: 'babel-loader',
             options: {
-                presets: ['@babel/preset-env', '@babel/preset-react'],
-                plugins: []
-            }
+                presets: [['@babel/preset-env', {targets: { browsers: ['> 5% in KR', 'last 2 chrome versions'],},debug: true,}], '@babel/preset-react'],
+                plugins: [],
+            },
         }],
     },
-
+    plugins:[
+        new webpack.LoaderOptionsPlugin({ debug: true }),
+    ],
     output: {
         filename: 'app.js',
         path: path.join(__dirname, 'dist'),
