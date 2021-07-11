0ed0fb34e4e4ebc50a31103a39e6f51171e6c94f
unknown <sjun8932@gmail.com>
2021-07-11 13:46:23 +0900

    숫자야구

--- Baseball/Baseball.jsx
+++ Baseball/Baseball.jsx
@@ -1,57 +1,102 @@
 const React = require('react');
-const {Component} = React;
-const Stayc = require('./Stayc');
+const { Component } = React;
+const Try = require('./Try');
 
-function getNumbers() {
-
+function getNumbers() { // 숫자 네 개를 겹치지 않고 랜덤하게 뽑는 함수
+    const candidate = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    const array = [];
+    for (let i = 0; i < 4; i += 1) {
+        const chosen = candidate.splice(Math.floor(Math.random() * (9 - i)), 1)[0];
+        array.push(chosen);
+    }
+    return array;
 }
 
 class Baseball extends Component {
 
-    state = {
-        result: '',
-        value: '',
-        answer: getNumbers(),
-        tries: [],
+    constructor(props) {
+        super(props);
+        this.state = {
+            result: '',
+            value: '',
+            answer: getNumbers(), // ex : [1,3,5,7]
+            tries: [],
+        };
+        console.log(this.state.answer);
+        this.onSubmitForm = this.onSubmitForm.bind(this);
+        this.onChangeInput = this.onChangeInput.bind(this);
+    }
+
+    onChangeInput(e) {
+        console.log(this.state.answer);
+        this.setState({
+            value: e.target.value,
+        });
     };
 
-    onSubmitForm = () => {
+    onSubmitForm(e) {
+        e.preventDefault();
+        if (this.state.value === this.state.answer.join('')) {
+            this.setState({
+                result: '홈런',
+                tries: [...this.state.tries, { try: this.state.value, result: '홈런!' }],
+            })
+            alert('게임을 다시 시작합니다!');
+            this.setState({
+                value: '',
+                answer: getNumbers(),
+                tries: []
+            });
+        } else {
+            const answerArray = this.state.value.split('').map((v) => parseInt(v));
+            let strike = 0;
+            let ball = 0;
+            if (this.state.tries.length >= 9) { // 10번 이상 틀렸을 때
+                this.setState({
+                    result: `10번 넘게 틀려서 실패! 답은 ${this.state.answer.join(',')}이었습니다.`,
+                });
+                alert('게임을 다시 시작합니다!');
+                this.setState({
+                    value: '',
+                    answer: getNumbers(),
+                    tries: []
+                });
+            } else {
+                for (let i = 0; i < 4; i += 1) {
+                    if (answerArray[i] === this.state.answer[i]) {
+                        strike += 1;
+                    } else if (this.state.answer.includes(answerArray[i])) {
+                        ball += 1;
+                    }
+                }
+                this.setState({
+                    tries: [...this.state.tries, { try: this.state.value, result: `${strike} 스트라이크, ${ball} 볼입니다.` }],
+                    value: '',
+                })
+            }
+        }
+    }
 
-    };
 
-    onChangeInput = () => {
-
-    };
-
-    stayc = [
-        {name: '시은', characteristic: '큐티'},
-        {name: '아이사', characteristic: '여시'},
-        {name: '수민', characteristic: '똘똘'},
-        {name: '세은', characteristic: '청순'},
-        {name: '재이', characteristic: '착함'},
-        {name: '윤',characteristic: '키가 큼'},
-        {name: '아이사', characteristic: '넘이쁨'}
-    ]
 
     render() {
         return (
             <>
                 <h1>{this.state.result}</h1>
-                <form onSubmit = {this.onSubmitForm}>
-                    <input maxLength = {4} value = {this.state.value} onChange = {this.onChangeInput}/>
+                <form onSubmit={this.onSubmitForm}>
+                    <input maxLength={4} value={this.state.value} onChange={this.onChangeInput} />
                 </form>
                 <div>시도: {this.state.tries.length}</div>
                 <ul>
-                    {this.stayc.map( (v,i) => {
+                    {this.state.tries.map((v, i) => {
                         return (
-                            <Stayc key={v.name + v.characteristic} value={v} index={i}/>
+                            <Try key={`${i + 1}차 시도:`} tryInfo={v} index={i} />
                         );
                     })}
                 </ul>
             </>
         );
     }
-
-}
+};
 
 module.exports = Baseball;
